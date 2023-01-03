aa3fcd4c60184b2b43ef7791d52fe5dccd81f89c
Jhosim-agudelo <jhosim.alejandro.agudelo.campo@umontreal.ca>
2023-01-03 17:29:24 -0500

    modified scanner and testingData

--- python finance project/scanner.py
+++ python finance project/scanner.py
@@ -5,26 +5,31 @@ import datetime
 
 
 client = RESTClient(config.API_KEY)
-test= ['AAL']
+
+tickers= pd.read_csv('tickersLt250M_copy.csv')
+tickers = list(tickers['ticker'])
 list = []
-
-
-start_date = '2022-12-05'
-end_date = '2022-12-09'
+# choose the range date that you want to use
+start_date = '2022-01-01'
+end_date = '2022-01-30'
 
 start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d')
 end_date = datetime.datetime.strptime(end_date, '%Y-%m-%d')
-print((end_date - start_date).days)
+#print((end_date - start_date).days)
 
-for day in range((end_date - start_date).days + 1):
-    current_day = start_date + datetime.timedelta(days=day)
-    current_day_str = current_day.strftime('%Y-%m-%d')
-    print(current_day_str)
-    for ticker in test:
+
+for ticker in tickers:
+    print('ticker: '+ ticker )
+    for day in range((end_date - start_date).days + 1):
+        current_day = start_date + datetime.timedelta(days=day)
+        current_day_str = current_day.strftime('%Y-%m-%d')
+
         list.append(client.get_daily_open_close_agg(ticker=ticker,date=current_day_str))
+
 
 
 list = pd.DataFrame(list)
 list = list.set_index('symbol')
+list.to_csv('openClose.csv"',index= True)
 
 print(list)
