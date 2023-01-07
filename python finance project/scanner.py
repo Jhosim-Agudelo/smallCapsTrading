d356931441122b6f93ca465bac88cd302df8ff8c
Jhosim-agudelo <jhosim.alejandro.agudelo.campo@umontreal.ca>
2023-01-07 02:22:58 -0500

    made some functions for the scanner file

--- python finance project/scanner.py
+++ python finance project/scanner.py
@@ -9,14 +9,6 @@ client = RESTClient(config.API_KEY)
 
 test = ['TSLA','AAPL','ZYNE','ZTEK']
 list = []
-# choose the range date that you want to use
-start_date = '2022-11-01'
-end_date = '2022-11-30'
-
-start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d')
-end_date = datetime.datetime.strptime(end_date, '%Y-%m-%d')
-#print((end_date - start_date).days)
-c = 0
 
 
 def get_data(stock,date):
@@ -26,26 +18,25 @@ def get_data(stock,date):
             print(stock,'No data on this day')
             return None
 
-def set_date_range(start_date,end_date):
-    for day in range((end_date - start_date).days + 1):
-        current_day = start_date + datetime.timedelta(days=day)
-        current_day_str = current_day.strftime('%Y-%m-%d')
-        return current_day_str
 
+def dataFrameSetup(current_day_str):
+    df = pd.DataFrame(list)
+    df = df.set_index('symbol')
+    df = df.loc[:,["from_","open","close","high","low","pre_market","after_hours","volume","status","otc"]]
+    df.to_csv(current_day_str+'.csv')
 
+def scanner(listOftickers,start_date,end_date):
+    start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d')
+    end_date = datetime.datetime.strptime(end_date, '%Y-%m-%d')
+    c=0
+    with ThreadPoolExecutor() as executor:
+        for stock in listOftickers:  
+            for day in range((end_date - start_date).days + 1):
+                c +=1
+                print('get request number: ',c, ' ticker: ',stock)
+                current_day = start_date + datetime.timedelta(days=day)
+                current_day_str = current_day.strftime('%Y-%m-%d')
+                executor.submit(get_data, stock, current_day_str)   
+    dataFrameSetup(current_day_str)
 
-with ThreadPoolExecutor() as executor:
-    for stock in test:  
-        for day in range((end_date - start_date).days + 1):
-            c +=1
-            print('get request number: ',c, ' ticker: ',stock)
-            current_day = start_date + datetime.timedelta(days=day)
-            current_day_str = current_day.strftime('%Y-%m-%d')
-            executor.submit(get_data, stock, current_day_str)
-
-
-df = pd.DataFrame(list)
-df = df.set_index('symbol')
-df = df.loc[:,["from_","open","close","high","low","pre_market","after_hours","volume","status","otc"]]
-df.to_csv(current_day_str+'.csv')
-print(df)
+scanner(listTickersLt250M,'2022-10-01','2022-10-31')
