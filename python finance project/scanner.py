44015488987765cc9c712aa93baa0a2454e95bbb
Jhosim-agudelo <jhosim.alejandro.agudelo.campo@umontreal.ca>
2023-01-06 22:01:08 -0500

    optimized scanner

--- python finance project/scanner.py
+++ python finance project/scanner.py
@@ -3,34 +3,49 @@ import config
 import pandas as pd
 import datetime
 from filterList import listTickersLt250M
-
+from concurrent.futures import ThreadPoolExecutor
 
 client = RESTClient(config.API_KEY)
 
-
-test2 = ['ZTEK']
-
-
+test = ['TSLA','AAPL','ZYNE','ZTEK']
 list = []
 # choose the range date that you want to use
-start_date = '2022-10-01'
-end_date = '2022-10-31'
+start_date = '2022-11-01'
+end_date = '2022-11-30'
 
 start_date = datetime.datetime.strptime(start_date, '%Y-%m-%d')
 end_date = datetime.datetime.strptime(end_date, '%Y-%m-%d')
 #print((end_date - start_date).days)
 c = 0
-for stock in listTickersLt250M:  
+
+
+def get_data(stock,date):
+        try:
+            return list.append(client.get_daily_open_close_agg(stock,date))
+        except: 
+            print(stock,'No data on this day')
+            return None
+
+def set_date_range(start_date,end_date):
     for day in range((end_date - start_date).days + 1):
-        c +=1
-        print('get request number: ',c, ' ticker: ',stock)
         current_day = start_date + datetime.timedelta(days=day)
         current_day_str = current_day.strftime('%Y-%m-%d')
-        try:
-            list.append(client.get_daily_open_close_agg(stock,date=current_day_str))
-        except: print(stock,'No data on this day')
+        return current_day_str
+
+
+
+with ThreadPoolExecutor() as executor:
+    for stock in test:  
+        for day in range((end_date - start_date).days + 1):
+            c +=1
+            print('get request number: ',c, ' ticker: ',stock)
+            current_day = start_date + datetime.timedelta(days=day)
+            current_day_str = current_day.strftime('%Y-%m-%d')
+            executor.submit(get_data, stock, current_day_str)
+
 
 df = pd.DataFrame(list)
 df = df.set_index('symbol')
+df = df.loc[:,["from_","open","close","high","low","pre_market","after_hours","volume","status","otc"]]
 df.to_csv(current_day_str+'.csv')
 print(df)
